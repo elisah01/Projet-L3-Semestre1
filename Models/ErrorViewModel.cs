8586be4932a0775ed917115b6823e0ac6b2d9f1c
elisah01 <bfaye428@gmail.com>
2025-12-16 19:33:23 +0000

    Initialisation du projet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Brasil_Burger.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
